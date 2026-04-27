b52d6f6f303abf8ba3876a1475bb778cdd16fcf2
unknown <jgabrielsouza0@gmail.com>
2026-04-27 14:01:51 -0300

    Adicionando o script de ordenação Bubble Sort

---
+++ bubble sort.py
@@ -0,0 +1,62 @@
+numeros = []
+
+while True:
+    num = (input("""Digite os números:
+Digite X se quiser finalizar """))
+    if num.upper() == "X":
+        break
+    try:
+        num = int(num)
+        numeros.append(num)
+    except ValueError:
+        print("""Digite apenas numeros ou "X""")
+
+for i in range (len(numeros)):
+    for n in range (len(numeros)-1):
+        if numeros[n+1] < numeros[n]:
+            temp = numeros[n+1]
+            numeros[n+1] = numeros[n]
+            numeros[n] = temp
+print(numeros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
